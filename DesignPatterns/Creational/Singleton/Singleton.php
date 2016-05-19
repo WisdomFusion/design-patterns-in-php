f2713d6007bf8fd11537009e55a7fc6f3bf1f398
WisdomFusion <WisdomFusion@gmail.com>
2016-05-20 01:06:11 +0800

    singleton

---
+++ DesignPatterns/Creational/Singleton/Singleton.php
@@ -0,0 +1,31 @@
+<?php
+namespace DesignPatterns\Creational\Singleton;
+
+class Singleton
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (null == static::$instance) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    private function __construct()
+    {
+
+    }
+
+    private function __clone()
+    {
+
+    }
+
+    private function __wakeup()
+    {
+        
+    }
+}
---
+++ tests/SingletonTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Tests;
+
+use DesignPatterns\Creational\Singleton\Singleton;
+
+class SingletonTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUniqeness()
+    {
+        $firstCall = Singleton::getInstance();
+        $this->assertInstanceOf('DesignPatterns\Creational\Singleton\Singleton', $firstCall);
+        $secondCall = Singleton::getInstance();
+        $this->assertSame($firstCall, $secondCall);
+    }
+
+    public function testNoConstructor()
+    {
+        $obj = Singleton::getInstance();
+
+        $refl = new \ReflectionObject($obj);
+        $meth = $refl->getMethod('__construct');
+        $this->assertTrue($meth->isPrivate());
+    }
+}
