616dd239a93ed7d4da57286a4b2761eb62cc081e
WisdomFusion <WisdomFusion@gmail.com>
2016-05-24 00:05:46 +0800

    add phpunit.xml; fix CarBuilder

--- DesignPatterns/Creational/Builder/CarBuilder.php
+++ DesignPatterns/Creational/Builder/CarBuilder.php
@@ -7,5 +7,52 @@ namespace DesignPatterns\Creational\Builder;
  */
 class CarBuilder implements BuilderInterface
 {
+    /**
+     * @var Parts\Car
+     */
+    protected $car;
 
+    /**
+     * @return void
+     */
+    public function addDoors()
+    {
+        $this->car->setPart('rightdoor', new Parts\Door());
+        $this->car->setPart('leftdoor', new Parts\Door());
+    }
+
+    /**
+     * @return void
+     */
+    public function addEngine()
+    {
+        $this->car->setPart('engine', new Parts\Engine());
+    }
+
+    /**
+     * @return void
+     */
+    public function addWheel()
+    {
+        $this->car->setPart('wheelLF', new Parts\Wheel());
+        $this->car->setPart('wheelRF', new Parts\Wheel());
+        $this->car->setPart('wheelLR', new Parts\Wheel());
+        $this->car->setPart('wheelRR', new Parts\Wheel());
+    }
+
+    /**
+     * @return void
+     */
+    public function createVehicle()
+    {
+        $this->car = new Parts\Car();
+    }
+
+    /**
+     * @return Parts\Car
+     */
+    public function getVehicle()
+    {
+        return $this->car;
+    }
 }
